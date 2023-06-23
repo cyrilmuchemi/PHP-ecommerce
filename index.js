aab93a54bca10cab4a7f46163fdb2b2e21144911
cyrilmuchemi <cyril.muchemi@gmail.com>
2023-06-23 12:53:49 +0300

    Complete special price section

--- index.js
+++ index.js
@@ -21,4 +21,14 @@ $(document).ready(function() {
             }
         }
     });
+    //isotope filter
+    var $grid = $(".grid").isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+    });
+
+    $(".button-group").on("click", "button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({filter:filterValue})
+    })
 });
