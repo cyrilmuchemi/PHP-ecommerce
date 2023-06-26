54de51c5b898d3c02de7d0091f816774292540e6
cyrilmuchemi <cyril.muchemi@gmail.com>
2023-06-26 10:47:59 +0300

    Complete site overview

--- index.js
+++ index.js
@@ -48,4 +48,19 @@ $(document).ready(function() {
             }
         }
     });
+    //blogs
+    $("#blogs .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+
+        }
+    });
 });
