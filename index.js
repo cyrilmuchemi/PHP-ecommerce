d3d34a8c4ca7ef7694355e7c118fd61d9d855868
cyrilmuchemi <cyril.muchemi@gmail.com>
2023-06-28 14:26:08 +0300

    Complete shopping cart

--- index.js
+++ index.js
@@ -67,9 +67,10 @@ $(document).ready(function() {
     //product quantity
     let $qty_up = $(".qty .qty-up");
     let $qty_down = $(".qty .qty-down");
-    let $input = $(".qty .qty_input");
+    //let $input = $(".qty .qty_input");
 
     $qty_up.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if($input.val() >= 1 && $input.val() <= 9){
             $input.val(function(i, oldval){
                 return ++oldval
@@ -78,6 +79,7 @@ $(document).ready(function() {
     });
 
     $qty_down.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
         if($input.val() > 1 && $input.val() <= 10){
             $input.val(function(i, oldval){
                 return --oldval
